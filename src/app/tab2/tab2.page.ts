c65ab584d8ff216e948b827f1aa58d55b8a4fd8e
edgardegantea <edgar.degante.a@gmail.com>
2020-10-07 16:50:56 -0500

    Modificación de Tab 1 y Tab 2

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import {Component, ViewChild} from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  // @ViewChild()
+
+  constructor() {}
+
+}
